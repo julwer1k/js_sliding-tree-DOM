8bf31baa8caba0142f87f549e1a81b3673b7815d
ArtyWhit <julwer1k@gmail.com>
2024-05-17 17:04:32 +0300

    make build

---
+++ index.a2a8255f.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".tree");document.querySelectorAll(".tree li").forEach(function(e){var t=document.createElement("span");e.prepend(t),t.append(t.nextSibling)}),e.addEventListener("click",function(e){if(e.preventDefault(),"SPAN"===e.target.tagName){var t=e.target.nextSibling;t&&(t.hidden=!t.hidden)}});
+//# sourceMappingURL=index.a2a8255f.js.map
